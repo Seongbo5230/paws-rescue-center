d2843f6d7a95ba0322094b09c8f4ca3d6947aa80
Seong Chang <you@example.com>
2020-03-29 22:21:38 -0700

    started on rendering a sign up form and navbar challenge

--- 6_form_handling/app.py
+++ 6_form_handling/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
+from forms import LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
 
 users = {
     "archie.andrews@email.com": "football4life",
@@ -13,13 +16,27 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
-        if users[email] == password:
-            return render_template("login.html", message="Successfully Logged In")
-        return render_template("login.html", message="Incorrect Email or Password")
-    return render_template("login.html")
+    form = LoginForm()
+
+    # request example
+    # if request.method == "POST":
+    #     email = request.form["email"]
+    #     password = request.form["password"]
+    #     if users[email] == password:
+    #         return render_template("login.html", message="Successfully Logged In")
+    #     return render_template("login.html", message="Incorrect Email or Password")
+
+    if form.validate_on_submit():
+        for u_email, u_password in users.items():
+            if u_email == form.email.data and u_password == form.password.data:
+                print("Email: " + form.email.data)
+                print("Password: " + form.password.data)
+                return render_template("login.html", message="Successfully Logged In!")
+        return render_template("login.html", form=form, message="Incorrect Email or Password...")
+    elif form.errors:
+        print(form.errors.items())
+
+    return render_template("login.html", form = form)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
